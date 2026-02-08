191ee87de8e4f66549746e42a4e7fbc7146704ea
pmurph11 <3gmurph@gmail.com>
2026-02-08 23:59:54 +0000

    com#2

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
+import sys
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, FPS
+
 def main():
-    pass
+    # Geeral setup 
+    pygame.init()
+
+    # Set up display
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Tetris")
+
+    # Game loop
+    running = True
+    clock = pygame.time.Clock()
+    while running:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        pygame.display.flip()
+    
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
